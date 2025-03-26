62832498ec7ec4af74bd1d4d5b5b151472968339
Kashope Akinsade <akinsadekash@gmail.com>
2025-03-26 18:28:00 -0400

    style + script

---
+++ script.js
@@ -0,0 +1,28 @@
+// referencing the id named code-output in html
+let output = document.getElementById("code-output");
+//printing the value of output in the console
+console.log("output= ",output)
+
+//declaration and initialization of the variable x with the value jordan
+let x = "Jordan";
+
+// declaration and initialization of the variable y with the value 3
+let y = 3;
+
+let z = true;
+
+x = "john";
+
+// permanently set the variable name to the value logan
+const name = "Logan";
+
+// unable to change the name value to roman
+name = "roman"
+
+// declaration of age
+let age;
+
+// initialization of age
+age = 33;
+
+output.innerHTML= name;
