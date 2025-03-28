668f23fdb67b8dc9f2cd0f00eedee6fd256ca667
Kashope Akinsade <akinsadekash@gmail.com>
2025-03-28 18:08:42 -0400

    data-types are go

---
+++ data-types/data-types.js
@@ -0,0 +1,22 @@
+let codeOutput = document.getElementById('code-output')
+console.log('the output is ,code-output');
+
+let hello = 'hello world!';
+console.log('this data type is a', typeof hello);
+
+let number = 3
+console.log('this data type is a', typeof number);
+
+let bool = false
+console.log('this is a', typeof bool);
+
+let nul;
+console.log("this is a", typeof nul);
+
+let obj = {"name":"john" }
+console.log("this is a", typeof obj);
+
+let myArr = [{"name":"marvin","name":"john"}]
+console.log("this is a", typeof myArr);
+
+codeOutput.innerHTML = 'check in console' //no semicolon needed
