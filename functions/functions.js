db5025a488c94b664b2982f92315a8f84ca678c5
Kashope Akinsade <akinsadekash@gmail.com>
2025-03-28 19:23:02 -0400

    functions are a go

--- functions/functions.js
+++ functions/functions.js
@@ -2,29 +2,22 @@ let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 let button = document.getElementById("submit");
 let output = document.getElementById("code-output");
-let num1 = input1.value;
-let num2 = input2.value;
-// function definition
-function addNumbers(x, y){
-    let input1 = document.getElementById("input1");
-let input2 = document.getElementById("input2");
-let button = document.getElementById("submit");
-let output = document.getElementById("code-output");
-let num1 = input1.value;
-let num2 = input2.value;
-    let solution = x + y;
+
+//function definition below
+function addNumbers( x, y){
+    let xNum = Number(x)
+    let yNum = Number(y)
+
+    let solution = xNum + yNum;
+
     output.innerHTML = solution
     return solution;
 }
+
 // function call
-// let x = addNumbers(num1, num2);
-// console.log("addNumbers returned the value of x", x);
-    button.addEventListener('click', function() {
-    let input1 = document.getElementById("input1");
-    let input2 = document.getElementById("input2");
-    let button = document.getElementById("submit");
-    let output = document.getElementById("code-output");
+button.addEventListener('click', function (){
+    console.log("working on it...")
     let num1 = input1.value;
     let num2 = input2.value;
     addNumbers(num1, num2);
-});
+})
